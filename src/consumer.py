56025202ea892aed17985da0cd18a20753923480
vbubblery <zhoujuncheng0116@gmail.com>
2022-09-19 16:59:48 +0200

    feat: gurobi consumer

--- src/consumer.py
+++ src/consumer.py
@@ -3,13 +3,18 @@ import os
 import json
 from dotenv import load_dotenv
 from pulp import LpProblem, GUROBI
-from src.logs import GurobiLogging
+from logs import GurobiLogging
+from azure.storage.fileshare import ShareFileClient
 
 load_dotenv()
 
+conn_str = ("DefaultEndpointsProtocol=https;AccountName=aksexternalglobal;"
+            "AccountKey=BpoiAAv0Sf0P5zdipcFa/seeAya7pl6f2v5BVf8MYqtsqE1wWgWXZ51j5jkUknF4B541o0b36fWHTMiLzQmAKw==;"
+            "EndpointSuffix=core.windows.net")
 
 def startConsumer():
     def _solve_model(dict_model, params, job_id):
+        os.system("killall -9 gurobi_cl")
         log_filename = f"gurobi_log_{job_id}"
         _, model = LpProblem.from_dict(dict_model)
         with GurobiLogging(log_filename=log_filename, job_id=job_id):
@@ -24,30 +29,52 @@ def startConsumer():
             )
         return model.to_dict()
 
-    def pull_model_from_sa(job_id: str):
-        return None
+    def pull_model_from_sa(file_name: str):
+        file_share_client = ShareFileClient.from_connection_string(
+                conn_str=conn_str,
+                share_name=f"gurobi-{os.getenv('PYTHON_ENV')}",
+                file_path=f"un-opt-models/{file_name}"
+            )
+        data = file_share_client.download_file()
+        return json.loads(data.readall())
+    
+    def push_model_to_sa(file_name: str, dict_model_out: dict):
+        try:
+            file_share_client = ShareFileClient.from_connection_string(
+                conn_str=conn_str,
+                share_name=f"gurobi-{os.getenv('PYTHON_ENV')}",
+                file_path=f"models/{file_name}",
+            )
 
-    def solve_model(time_limit: str = "0", gap_rel: str = "0", job_id: str = "0"):
-        os.system("killall -9 gurobi_cl")
-        params = {"timeLimit": time_limit, "gapRel": gap_rel}
-        dict_model_in = pull_model_from_sa(job_id)
-        dict_model_out = _solve_model(dict_model_in, params, job_id=job_id)
+            file_share_client.upload_file(json.dumps(dict_model_out))
+        except Exception as error:
+            print(error)
+            # logger.exception(f"Failed to upload solution of job {job_id}")
+        # else:
+        #     logger.info(f"Solution of job {job_id} has been uploaded.")
 
-        return dict_model_out
+    def solve_model(job_id: str):
+        json_models = pull_model_from_sa(job_id)
+        dict_res = {}
+        for model_id, json_model in json_models.items():
+            params = {
+                "timeLimit": json_model["timeLimit"],
+                "gapRel": json_model["gapRel"],
+            }
+
+            dict_res[model_id] = _solve_model(json_model, params, job_id=job_id)
+
+        return dict_res
 
     def callbackFunctionForQueue(channel, method, properties, body):
         print(f"Got a message from Queue C: {body}")
         print(f"message id: {properties.message_id}")
         message = json.loads(body)
 
-        time_limit = message.get("time_limit", "0")
-        gap_rel = message.get("gap_rel", "0")
         job_id = properties.message_id
-        solve_model(
-            time_limit=time_limit,
-            gap_rel=gap_rel,
-            job_id=job_id
-        )
+        dict_model_out = solve_model(job_id=job_id)
+
+        push_model_to_sa(file_name=job_id, dict_model_out=dict_model_out)
 
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
